fa5f606db4373b16516d26986a12374a78000572
SpikyTV <30ax30@gmail.com>
2026-02-28 18:42:28 +0100

    + general rework

--- solver.py
+++ solver.py
@@ -1,3 +1,4 @@
+import logging
 import csv
 import os
 
@@ -56,6 +57,20 @@ class Solver:
     
     def __init__(self):
         
+        self.logger = logging.getself.logger()
+        self.logger.setLevel(logging.INFO)
+
+        formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
+
+        console = logging.StreamHandler()
+        console.setFormatter(formatter)
+
+        file = logging.FileHandler("Solver.log")
+        file.setFormatter(formatter)
+
+        self.self.logger.addHandler(console)
+        self.logger.addHandler(file)
+        
         self.counter = 0
         self.diff = 0
         self.diff_k = 0
@@ -93,20 +108,21 @@ class Solver:
         self.client.loop_start()
 
         print("MQTT server running...")
-    
-    def get_nodes(self):
-        # returns node dictionary
-        return self.nodes.values()
-
+        
+#====================================================================================
+#            comms basic section (mqtt)
+#==================================================================================== 
     def on_connect(self, client, userdata, flags, reason_code, properties):
         # MQTT on_connect callback
         if reason_code == 0:
-            print("Connected to MQTT broker")
-            self.client.subscribe(self.TOPIC)
-            self.client.subscribe(self.TOPIC3)
-            self.client.subscribe(self.TOPIC5)
+            self.logger.info("Connected to MQTT broker")
+            self.client.subscribe("hub/solver/cmd/measure_anchor")
+            self.client.subscribe("hub/solver/cmd/toggle_tag_ranging")
+            self.client.subscribe("hub/solver/cmd/toggle_tag_sim")
+            self.client.subscribe("hub/esp/+/ranging")
+            self.client.subscribe("hub/esp/+/status")
         else:
-            print(f"Connection failed (reason_code={reason_code})")
+            self.logger.error(f"Connection failed (reason_code={reason_code})")
         
     def on_message(self, client, userdata, msg):
         # MQTT on_message callback
@@ -114,134 +130,98 @@ class Solver:
         try:
             topic_parts = msg.topic.split("/")
             payload = json.loads(msg.payload.decode("utf-8"))
-            self.msg_queue.put((topic_parts[1], topic_parts[2], payload))
+            self.msg_queue.put((topic_parts, payload))
         except Exception as e:
-            print("Error processing message:", e)
-            
+            self.logger.error("Error processing message: %s", e)
+
     def message_processor(self):
         # processes MQTT messages
         while True:
-            node_id, in_topic, data = self.msg_queue.get()  # blocks until message is available
+            topic_parts, data = self.msg_queue.get()  # blocks until message is available
 
-            print(f"Processing {in_topic} from {node_id}")
-            
-            if in_topic == "register":
-                threading.Thread(target=self.register_client, args=(data,), daemon=True).start()
-            elif in_topic == "data":
-                threading.Thread(target=self.receive_data, args=(data,), daemon=True).start()
-            elif in_topic == "UICommand":
-                threading.Thread(target=self.handle_UIcommand, args=(data,), daemon=True).start()
+            if topic_parts[1] == "solver":
+                if topic_parts[3] == "measure_anchor":
+                    threading.Thread(target=self.measure, args=("anch",), daemon=True).start()
+                elif topic_parts[3] == "toggle_tag_ranging":
+                    threading.Thread(target=self.measure, args=("tag",), daemon=True).start()
+                elif topic_parts[3] == "toggle_tag_sim":
+                    topic = f"hub/esp/bcast/cmd/move"
+                    self.client.publish(topic, "", qos=1)
+                else:
+                    self.logger.error(f"Error processing command: {topic_parts[3]}")
+            elif topic_parts[1] == "esp":
+                if topic_parts[3] == "ranging":
+                    self.process_ranging(data)
+                elif topic_parts[3] == "status":
+                    self.update_node(data)
+                else:
+                    self.logger.error(f"Error processing esp data: {topic_parts[3]}")
             else:
-                print(f"unknown topic: {in_topic}")
-
-    def register_client(self, data):
-        # registers new ESP32 (UWB) node to the solver
-        node_id = data.get('esp_id')
-        tag = data.get('tag')
-        x = data.get('x')
-        y = data.get('y')
+                self.logger.error(f"Error processing topic: {topic_parts}")
+#====================================================================================
+#            comms section (mqtt)
+#==================================================================================== 
+    def send_UIData(self):
+        payload = {"nodes": [asdict(node) for node in self.nodes.values()]}
         
-        self.nodes[node_id] = Nodes(node_id, tag, (x, y))
+        topic = f"hub/solver/solution"
+        self.client.publish(topic, json.dumps(payload), retain=True, qos=1)
         
-        # send updated positions to UI       
-        self.send_UIData()
+    def update_node(self, data):
+        node_id = data.get("esp_id")
+        tag = data.get("tag")
+        real = data.get("cords")
         
-        #move to log
-        print(f"[SERVER] Registered client: {self.nodes[node_id]}")
-    
-    def receive_data(self, data):
-        # recieves data from nodes
-        node_id = data.get('esp_id')
-        node = self.nodes.get(node_id)
-        
-        x = data.get('x')
-        y = data.get('y')
-        
-        
-        node.real = (x, y)
-        node.distance = data.get('distance')
-
-        node.dispersion = data.get('dispersion')
-        
-        print(f"incoming data dist: {data.get('distance')}, disp {data.get('dispersion')}")
-        
-        if data.get('tag') == "tag":
+        if node_id in self.nodes:
+            node = self.nodes[node_id]
+            node.tag = tag
+            node.real = real
+        else: 
+            self.nodes[node_id] = Nodes(node_id, tag, real)
             
-            node = self.nodes.get("Kalman")
-            node.real = (x, y)
-            node.distance = data.get('distance')
-            node.dispersion = data.get('dispersion')
-                
-            self.estimate_tag_position()
+        self.send_UIData()
+    
+    def process_ranging(self, data):
+        node = self.nodes.get(data.get("esp_id"))
         
-        #signal Data Recieved
-        print(f"[SERVER] Data received from {node_id}")
+        node.distance = data.get("mean_distances")
+        node.dispersion = data.get("std_deviations")
+        
+        if data.get("tag") == "tag":
+            node.real = data.get("cords")
+            node = self.nodes.setdefault("Kalman", Nodes("Kalman", "tag", data.get("cords")))
+            node.real = data.get("cords")
+            self.estimate_tag_position()
+            
         self.measurement_done.set()
         
-    def measure_anchor(self):
-        # starts mesurments between anchors
-        # triggers position estimation
-        print("[SERVER] Starting measurements...")
+    def measure(self, tag):
+        self.logger.info("[SERVER] Starting measurements...")
         
-        payload = {
-            "type": "command",
-            "command": "uwb"
-        }
+        nodes = [node for node in self.nodes.values() if node.tag == tag]
         
-        topic = "hub/uwb/command"
-
-        #on connect sends register topic with 1 send garanteed
-        self.client.publish(topic, json.dumps(payload), qos=1)
-
-        anchor_nodes = [node for node in self.nodes.values() if node.tag == "anch"]
-        
-        time.sleep(2)
-        
-        for node in anchor_nodes:
-            print(f"\n[SERVER] Sending measurement command to client: {node.id}...")
+        for node in nodes:
+            self.logger.info(f"\n[SERVER] Sending measurement command to client: {node.id}...")
             
-            payload = {"type": "command", "command": "start_measurement"}
-            topic = f"hub/{node.id}/command"
-            
+            topic = f"hub/esp/{node.id}/measure"
+            self.client.publish(topic, "", qos=1)
             self.measurement_done.clear()
-            self.client.publish(topic, json.dumps(payload), qos=1)
             
             # Wait until ESP returns data or 1:30 min passes
             finished = self.measurement_done.wait(timeout=90)
             
             if not finished:
-                print(f"[SERVER] Failed to receive data from node: {node.id}")  
+                self.logger.error(f"[SERVER] Failed to receive data from node: {node.id}")  
                 
-            
-        self.estimate_anchor_positions_scipy()
-                
-    def measure_tag(self):
-        # starts mesurments between tag and anchors
-        # triggers position estimation for tag
-        print("[SERVER] Starting measurements...")
+        if tag == "anch":        
+            self.estimate_anchor_positions_scipy()
+        else:
+            #add one time visual ndoe
+            self.estimate_tag_position()
 
-        tag_nodes = [node for node in self.nodes.values() if node.tag == "tag"]
-        
-        for node in tag_nodes:
-            
-            self.nodes["Kalman"] = Nodes("Kalman", "tag", node.real)
-            
-            print(f"\n[SERVER] Sending measurement command to client: {node.id}...")
-            
-            payload = {"type": "command", "command": "start_tag"}
-            topic = f"hub/{node.id}/command"
-            
-            self.measurement_done.clear()
-            self.client.publish(topic, json.dumps(payload), qos=1)
-            
-            # Wait until ESP returns data or 1:30 min passes
-            finished = self.measurement_done.wait(timeout=90)
-            
-            if not finished:
-                print(f"[SERVER] Failed to receive data from node: {node.id}")
-                
-        self.estimate_tag_position()
-  
+#====================================================================================
+#            filters and estimation section
+#====================================================================================  
     def build_matrices(self):
         # build matrix for anchors to be used in position estimation
         anchor_nodes = [node for node in self.nodes.values() if node.tag == "anch"]
@@ -252,42 +232,7 @@ class Solver:
         dispersion_matrix = [node.dispersion for node in anchor_nodes]
 
         return np.array(distance_matrix), np.array(dispersion_matrix)
-  
-    def Correction_pos(self):
-        #SciPy align calculated positions with real positions
-        
-        #only use anchors
-        anchor_nodes = [node for node in self.nodes.values() if node.tag == "anch"]
-        
-        # Build real and calculated matrices from nodes
-        real = np.array([node.real for node in anchor_nodes])        # shape: (N_anchors, 2)
-        calc = np.array([node.calculated for node in anchor_nodes])  # shape: (N_anchors, 2)
-        
-        # Compute centroids
-        r_centroid = real.mean(axis=0)
-        c_centroid = calc.mean(axis=0)
-        
-        # Center the coordinates
-        R = real - r_centroid
-        C = calc - c_centroid
-        
-        #RTC
-        # Orthogonal Procrustes: find best rotation
-        rotation, _ = orthogonal_procrustes(C, R)
-
-        # Compute translation
-        translation = r_centroid - c_centroid @ rotation
-
-        # Apply transformation
-        corrected = calc @ rotation + translation
-        
-        # Write back corrected positions to nodes
-        for node, pos in zip(anchor_nodes, corrected):
-            node.calculated = tuple(pos)
-        
-        # send updated positions to UI       
-        self.send_UIData()
-        
+    
     def estimate_anchor_positions_scipy(self, dim=2):
         # Estimate anchor positions from pairwise distances using weighted nonlinear least squares.
         # magic happens here
@@ -333,75 +278,7 @@ class Solver:
             
         #correct the RTC of calculation
         self.Correction_pos()
-        
-    def filter_kalman(self, x, y):
-
-        # first setup kalman
-        if self.kf is None:
-            kf = KalmanFilter(dim_x=4, dim_z=2)  # state: [x,y,vx,vy], meas: [x,y]
-            kf.x = np.array([x, y, 0.0, 0.0], dtype=float)
-            kf.H = np.array([[1, 0, 0, 0],[0, 1, 0, 0]], dtype=float) # means x and y relate to state x and y
-            # [1x, 0y, 0vx, 0vy],[0x, 1y, 0vx, 0vy]
-
-            kf.P = np.eye(4, dtype=float) * 1.0
-
-            # Measurement noise needs to be set up 
-            meas_std = 0.19
-            kf.R = np.eye(2, dtype=float) * (meas_std ** 2)
-
-            self.kf = kf
-            self.kf_last_t = time.time()
-            return (x, y)
-
-        # time
-        now = time.time()
-        dt = now - self.kf_last_t
-        self.kf_last_t = now
-
-        # if timing goes bad
-        dt = float(max(1e-3, min(0.5, dt)))
-
-        self.kf.F = np.array([[1, 0, dt, 0],
-                            [0, 1, 0, dt],
-                            [0, 0, 1,  0],
-                            [0, 0, 0,  1]], dtype=float)
-
-        accel_std = 8.0  # m/s^2; can horse acelerate this fast?
-        q = accel_std ** 2
-        dt2 = dt * dt
-        dt3 = dt2 * dt
-        dt4 = dt2 * dt2
-        self.kf.Q = q * np.array([[dt4/4, 0,     dt3/2, 0],
-                             [0,     dt4/4, 0,     dt3/2],
-                             [dt3/2, 0,     dt2,   0],
-                             [0,     dt3/2, 0,     dt2]], dtype=float)
-
-        z = np.array([x, y], dtype=float)
-
-        self.kf.predict()
-        self.kf.update(z)
-
-        x_f = self.kf.x[0]
-        y_f = self.kf.x[1]
-        vx  = self.kf.x[2]
-        vy  = self.kf.x[3]
-        
-        #for visualisation
-        node = self.nodes.get("Kalman")
-        node.calculated = (x_f, y_f)
-        
-        return (x_f, y_f)
-
-    def append_csv_row(self, csv_path: str, row: dict):
-        file_exists = os.path.exists(csv_path)
-        fieldnames = list(row.keys())
-
-        with open(csv_path, "a", newline="", encoding="utf-8") as f:
-            writer = csv.DictWriter(f, fieldnames=fieldnames)
-            if not file_exists:
-                writer.writeheader()
-            writer.writerow(row)
-        
+    
     def estimate_tag_position(self, dim=2):
         # Estimate tag position using known anchor locations.
         # magic also happens here
@@ -445,51 +322,105 @@ class Solver:
             self.diff += math.hypot(x_real - x, y_real - y)
             self.diff_k += math.hypot(x_real - x_f, y_real - y_f)
             
-            
-            
-            if self.counter%5 == 0:
-            
-                row = {
-                    "timestamp": time.time(),
-                    "normal_diff": (self.diff/5),
-                    "kalman_diff": (self.diff_k/5)
-                }
-                
-                self.diff = 0
-                self.diff_k = 0
-                
-                self.counter =0
-                
-                csv_path = f"tables/kalman/mesurments-kalman-diff.csv"
-                self.append_csv_row(csv_path, row)
-                
             self.counter += 1
             
             # send update to UI
             self.send_UIData()
             
-            
-            
-            
-    
-    def send_UIData(self):
-        payload = {"nodes": [asdict(node) for node in self.nodes.values()]}
+    def filter_kalman(self, x, y):
+
+        # first setup kalman
+        if self.kf is None:
+            kf = KalmanFilter(dim_x=4, dim_z=2)  # state: [x,y,vx,vy], meas: [x,y]
+            kf.x = np.array([x, y, 0.0, 0.0], dtype=float)
+            kf.H = np.array([[1, 0, 0, 0],[0, 1, 0, 0]], dtype=float) # means x and y relate to state x and y
+            # [1x, 0y, 0vx, 0vy],[0x, 1y, 0vx, 0vy]
+
+            kf.P = np.eye(4, dtype=float) * 1.0
+
+            # Measurement noise needs to be set up 
+            meas_std = 0.19
+            kf.R = np.eye(2, dtype=float) * (meas_std ** 2)
+
+            self.kf = kf
+            self.kf_last_t = time.time()
+            return (x, y)
+
+        # time
+        now = time.time()
+        dt = now - self.kf_last_t
+        self.kf_last_t = now
+
+        # if timing goes bad
+        dt = float(max(1e-3, min(0.5, dt)))
+
+        self.kf.F = np.array([[1, 0, dt, 0],
+                            [0, 1, 0, dt],
+                            [0, 0, 1,  0],
+                            [0, 0, 0,  1]], dtype=float)
+
+        accel_std = 3.0  # m/s^2; can horse acelerate this fast?
+        q = accel_std ** 2
+        dt2 = dt * dt
+        dt3 = dt2 * dt
+        dt4 = dt2 * dt2
+        self.kf.Q = q * np.array([[dt4/4, 0,     dt3/2, 0],
+                             [0,     dt4/4, 0,     dt3/2],
+                             [dt3/2, 0,     dt2,   0],
+                             [0,     dt3/2, 0,     dt2]], dtype=float)
+
+        z = np.array([x, y], dtype=float)
+
+        self.kf.predict()
+        self.kf.update(z)
+
+        x_f = self.kf.x[0]
+        y_f = self.kf.x[1]
+        vx  = self.kf.x[2]
+        vy  = self.kf.x[3]
         
-        topic = f"hub/server/UIData"
-        self.client.publish(topic, json.dumps(payload), qos=1)
-    
-    def handle_UIcommand(self, data):
-        # command handeling
-        command = data.get("command")
+        #for visualisation
+        node = self.nodes.get("Kalman")
+        node.calculated = (x_f, y_f)
         
-        print(f"executing command {command}")
+        return (x_f, y_f)
+  
+    def Correction_pos(self):
+        #SciPy align calculated positions with real positions
         
-        if command == "measure_anchors":
-            self.measure_anchor()
-        elif command == "measure_tags":
-            self.measure_tag()
-        elif command == "send_nodes":
-            self.send_UIData()
+        #only use anchors
+        anchor_nodes = [node for node in self.nodes.values() if node.tag == "anch"]
+        
+        # Build real and calculated matrices from nodes
+        real = np.array([node.real for node in anchor_nodes])        # shape: (N_anchors, 2)
+        calc = np.array([node.calculated for node in anchor_nodes])  # shape: (N_anchors, 2)
+        
+        # Compute centroids
+        r_centroid = real.mean(axis=0)
+        c_centroid = calc.mean(axis=0)
+        
+        # Center the coordinates
+        R = real - r_centroid
+        C = calc - c_centroid
+        
+        #RTC
+        # Orthogonal Procrustes: find best rotation
+        rotation, _ = orthogonal_procrustes(C, R)
+
+        # Compute translation
+        translation = r_centroid - c_centroid @ rotation
+
+        # Apply transformation
+        corrected = calc @ rotation + translation
+        
+        # Write back corrected positions to nodes
+        for node, pos in zip(anchor_nodes, corrected):
+            node.calculated = tuple(pos)
+        
+        # send updated positions to UI       
+        self.send_UIData()
+
+        
         
 if __name__ == "__main__":
     
